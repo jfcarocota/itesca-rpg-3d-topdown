fbdc5d78cbe26139da4e01a972d852416b27a358
jfcarocota <jfcarocota@gmail.com>
2019-11-16 10:37:11 -0700

    player move

---
+++ Assets/Scripts/Player/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Movement;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    protected float moveSpeed;
+   
+   void Update()
+   {
+       Move();
+   }
+
+   public virtual void Move()
+   {
+       Movement.Move(transform, moveSpeed, Movement.AxisDelta);
+   }
+}
---
+++ Assets/Scripts/Player/Core.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Movement
+{
+    public static class Movement
+    {
+        public static Vector3 Axis {get => new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));}
+
+        public static Vector3 AxisDelta 
+        {
+            get => new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime;
+        }
+
+        public static void Move(Transform t, float moveSpeed, Vector3 dir)
+        {
+            t.Translate(dir * moveSpeed);
+        }
+    }
+}
